b09eb7d7b1ad83581353e89e829b8e82a172ad2e
Brad R <brad.w.rollinson@gmail.com>
2021-09-15 18:57:36 -0400

    remove bcrypt from user route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -28,15 +28,12 @@ router.post('/login', async (req, res) => {
       return;
     }
 
-    // use `bcrypt.compare()` to compare the provided password and the hashed password
-    const validPassword = await bcrypt.compare(
-      req.body.password,
-      userData.password
-    );
+    const validPassword = await userData.checkPassword(req.body.password);
 
-    // if they do not match, return error message
     if (!validPassword) {
-      res.status(400).json({ message: 'Incorrect email or password, please try again' });
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
       return;
     }
 
