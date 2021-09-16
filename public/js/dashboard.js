16b4d160d4c6396ff4ec7b838b3a3e517faaab14
Lauren Goss <laurenlgoss98@gmail.com>
2021-09-15 21:21:16 -0400

    pseudocoded dashboard.js

---
+++ public/js/dashboard.js
@@ -0,0 +1,15 @@
+// Add Job button
+    // Fetch POST {title, link, company_name, description, salary_information, contact_information, application_status, additional_comments}
+
+    // Refresh screen to load new job
+
+// Delete Job button
+    // Fetch DELETE
+
+    // Refresh screen to load without job
+
+// Job button
+    // Redirect user to job.html
+
+// Calendar button
+    // Redirect user to calendar.html
